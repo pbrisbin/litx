63df1e008a83a6f58c71ec6ab04a4f734ced0608
patrick brisbin <pbrisbin@gmail.com>
2022-11-17 16:32:54 -0500

    Avoid false-passing in spec

--- test/LitXSpec.hs
+++ test/LitXSpec.hs
@@ -6,12 +6,15 @@ import LitX.Prelude
 
 import LitX
 import Test.Hspec
+import UnliftIO.Directory (doesFileExist, removeFile)
 
 spec :: Spec
 spec = do
     describe "litx" $ do
         it "processes our example correctly" $ do
             let tmp = "/tmp/litx-test.bash"
+            exists <- doesFileExist tmp
+            when exists $ removeFile tmp
 
             litx ["--input", "files/example.md", "--output", tmp]
 
